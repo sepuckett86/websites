df59e5fb93e94c23994d279d018102af7c0c0302
sepuckett86 <sepuckett86@gmail.com>
2019-03-31 20:46:01 -0700

    svg wave play and fix navbar scrolling to mission with jquery

--- BSF/js/BSF.js
+++ BSF/js/BSF.js
@@ -1,11 +1,13 @@
-// To add CSS class depending on scroll location
-// To add box shadow after scroll
+
 // Will do after page loaded
 $(document).ready(function(){
-  var scrollLocation = 0;
-  // Returns
+// Scrolling with offset of navbar
+
+// To add CSS class depending on scroll location
+// To add box shadow after scroll
+
+  var scrollLocation = $(window).scrollTop();
   var introHeight = $('#intro').height();
-  console.log(introHeight);
   // On scroll
   $(window).scroll(function(){
     // Set scroll location
